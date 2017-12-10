2dcfceabdc870dda02fd4c67ea405766d86bce9c
Stephen Sherratt <sfsherratt@gmail.com>
2017-12-10 16:52:07 +0000

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd $DIR
+    jekyll build
+    s3cmd sync _site/* s3://gridbugs.org
+fi
