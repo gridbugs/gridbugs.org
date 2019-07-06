b0c55ef212ed11ce8fd663b1627087e2ac5eef74
Stephen Sherratt <stephen@sherra.tt>
2019-07-06 22:00:28 +0100

    Work in progress custom image tag

---
+++ _plugins/image.rb
@@ -0,0 +1,21 @@
+module Jekyll
+    class ImageTag < Liquid::Tag
+        def initialize(tag_name, filename, tokens)
+            super
+            parts = filename.split(' ')
+            @filename = parts[0].strip
+            @attributes = parts[1..-1].join(' ')
+        end
+
+        def render(context)
+            image_dir = "/images"
+            page_url = context.registers[:page].url
+            base_url = context.registers[:site].baseurl
+            src = "#{base_url}#{image_dir}#{page_url}#{@filename}"
+            alt = File.basename(@filename, File.extname(@filename))
+            "<img src=\"#{src}\" alt=\"#{alt}\" #{@attributes}>"
+        end
+    end
+end
+
+Liquid::Template::register_tag('image', Jekyll::ImageTag)
