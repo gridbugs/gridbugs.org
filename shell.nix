209d563e498a879f6e059ac57e18cbc03e92930c
Stephen Sherratt <stephen@sherra.tt>
2022-12-24 11:22:35 +1000

    Add gemset

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
+let
+  ruby = pkgs.ruby_3_1;
+  gems = pkgs.bundlerEnv {
+    name = "gridbugs.org";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in pkgs.mkShell {
   packages = with pkgs; [
-    ruby_3_1
+    ruby
+    gems
   ];
 }
