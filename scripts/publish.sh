b6c1808ea7141e332c603da8a11be467ba7901cf
Stephen Sherratt <stephen@sherra.tt>
2023-01-07 19:48:09 +1100

    Publish script assumes jekyll is a command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,7 +10,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     cd $DIR
-    ${BUNDLE:-bundle} exec jekyll build --config _config.yml,_config_gridbugs.yml
+    jekyll build --config _config.yml,_config_gridbugs.yml
     rm -f _site/*.core
     rsync -Pav _site/ gridbugs_nfs:
 fi
