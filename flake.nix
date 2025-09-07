4d73512a6b8a2e1f94856c37968010029a26808b
Stephen Sherratt <stephen@sherra.tt>
2025-09-07 10:12:50 +1000

    No need to build zola from source

--- flake.nix
+++ flake.nix
@@ -2,34 +2,11 @@
   description = "gridbugs.org dev environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustPlatform = pkgs.makeRustPlatform {
-          rustc = pkgs.rust-bin.stable.latest.default;
-          cargo = pkgs.rust-bin.stable.latest.default;
-        };
-        zola = rustPlatform.buildRustPackage rec {
-          pname = "zola";
-          version = "0.21.0";
-          src = pkgs.fetchFromGitHub {
-            owner = "getzola";
-            repo = "zola";
-            rev = "v${version}";
-            hash = "sha256-+/0MhKKDSbOEa5btAZyaS3bQPeGJuski/07I4Q9v9cg=";
-          };
-
-          cargoHash = "sha256-K2wdq61FVVG9wJF+UcRZyZ2YSEw3iavboAGkzCcTGkU=";
-
-          nativeBuildInputs = [ pkgs.pkg-config ];
-
-          buildInputs = [ pkgs.openssl pkgs.zlib ];
-        };
+      let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
         devShell = mkShell { buildInputs = [ zola ffmpeg imagemagick ]; };
       });
